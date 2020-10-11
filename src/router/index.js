bd340df28df1950ce97b83d6620d5ee59564d1af
InGee Kim <ingee.kim@gmail.com>
2020-10-11 15:32:53 +0900

    Main-view: Implement basic layout (url: /, /calendar)

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Calendar from '../views/Calendar.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/calendar',
+    name: 'Calendar',
+    component: Calendar
   },
   {
     path: '/about',
@@ -21,7 +27,6 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
---
+++ src/views/Calendar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <v-date-picker
+      v-model="today"
+      :events="workoutDays"
+      :first-day-of-week="1"
+      event-color="red"
+      full-width
+    ></v-date-picker>
+    <v-fab-transition>
+      <v-btn
+        color="primary"
+        class="mb-10"
+        fab
+        absolute
+        bottom
+        right
+      >
+        <v-icon>
+          mdi-plus
+        </v-icon>
+      </v-btn>
+    </v-fab-transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      today: this.makeTodayStr(),
+      workoutDays: [
+        this.makeDateStr(2020, 10, 1),
+        this.makeDateStr(2020, 10, 3),
+        this.makeDateStr(2020, 10, 5),
+        this.makeDateStr(2020, 10, 7),
+        this.makeDateStr(2020, 10, 9),
+      ]
+    }
+  },
+  methods: {
+    makeDateStr (year, month, day) {
+      return (new Date(`${year}-${month}-${day}`)).toISOString().substr(0, 10)
+    },
+    makeTodayStr () {
+      return (new Date()).toISOString().substr(0, 10)
+    }
+  }
+}
+</script>
